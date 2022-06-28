e06202982078c04539951e05798a1129d8e73932
Sneha <98872101+Snehaheidi@users.noreply.github.com>
2022-06-28 16:44:46 +0530

    Add files via upload

---
+++ BankMain.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+
+public class BankMain
+{
+   public static void main(String[] args) throws IOException {
+      BankApplication ba = new BankApplication();
+      ba.BankApp();
+   }
+}
+//OUTPUT:
+/*
+ *_*_*_*_*_*_  Welcome To STC Bank  *_*_*_*_*_*_
+
+1).Create new Account 	 2).Login 	 3).Exit
+2
+Enter the your Holder Name :
+sneha
+Enter your Password :
+1234
+Welcome sneha
+1). Transfer Money
+ 2). View Balance
+ 3). Deposit
+ 4). WithDraw
+ 5). LogOuT
+1
+Enter the Receiver Id :
+2
+Enter the Amount :
+2000
+Your Debited Successfully...
+----------------------------------------------
+Transaction Successfully...
+Welcome sneha
+1). Transfer Money
+ 2). View Balance
+ 3). Deposit
+ 4). WithDraw
+ 5). LogOuT
+2
+_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
+Holder_Name        : sneha
+Account Balance is : 6000
+----------THANK YOU------------
+>>>>>>>>Welcome Back<<<<<<<<
+Welcome sneha
+1). Transfer Money
+ 2). View Balance
+ 3). Deposit
+ 4). WithDraw
+ 5). LogOuT
+3
+Enter the Deposit Amount :
+1000
+Your Amount Credited...
+Thank you
+Welcome sneha
+1). Transfer Money
+ 2). View Balance
+ 3). Deposit
+ 4). WithDraw
+ 5). LogOuT
+4
+Enter the WithDraw Amount
+500
+Your Amount Debited....
+------------------------------
+Welcome sneha
+1). Transfer Money
+ 2). View Balance
+ 3). Deposit
+ 4). WithDraw
+ 5). LogOuT
+5
+
+ *_*_*_*_*_*_  Welcome To STC Bank  *_*_*_*_*_*_
+
+1).Create new Account 	 2).Login 	 3).Exit
+3
+
+ */
