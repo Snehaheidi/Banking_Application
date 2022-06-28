4b3955e7cf4d6ddfa8cfa362a2d87710ad6b2205
Sneha <98872101+Snehaheidi@users.noreply.github.com>
2022-06-28 16:39:13 +0530

    Add files via upload

---
+++ BankApplication.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BankApplication {
+    public static void BankApp() throws IOException {
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+        String name = "";
+        int passcode = 0;
+        System.out.println("\n *_*_*_*_*_*_  Welcome To STC Bank  *_*_*_*_*_*_\n");
+        System.out.println("1).Create new Account \t 2).Login \t 3).Exit");
+        int choice = Integer.parseInt(scanner.readLine());
+        switch(choice)
+        {
+            case 1:
+                System.out.println("Enter the Holder Name : ");
+                name = scanner.readLine();
+                System.out.println("Enter New Password ");
+                passcode = Integer.parseInt(scanner.readLine());
+                if(LoginCreate.createAccount(name,passcode))
+                {
+                    System.out.println("Now you Login...<3");
+                }
+                break;
+            case 2:
+                System.out.println("Enter the your Holder Name : ");
+                name = scanner.readLine();
+                System.out.println("Enter your Password :");
+                passcode = Integer.parseInt(scanner.readLine());
+                if(LoginCreate.login(name,passcode))
+                {
+                    System.out.println("Thank You...!");
+                }
+                break;
+            case 3:
+                System.exit(0);
+                break;
+        }
+    }
+}
---
+++ BankMain.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class BankMain
+{
+   public static void main(String[] args) throws IOException {
+      BankApplication ba = new BankApplication();
+      ba.BankApp();
+   }
+}
