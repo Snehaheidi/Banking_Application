9331451d23617017108b020be7d5f4cb962de61b
Sneha <98872101+Snehaheidi@users.noreply.github.com>
2022-06-28 16:44:24 +0530

    Delete BankMain.java

--- BankMain.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class BankMain
-{
-   public static void main(String[] args) throws IOException {
-      BankApplication ba = new BankApplication();
-      ba.BankApp();
-   }
-}
